778dbfe10b9e55fcfa7f029b17721af54a12655e
e-barr <31676765+e-barr@users.noreply.github.com>
2018-11-27 22:16:55 -0600

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import './App.css';
 import TodoList from './TodoList.js'
 
 class App extends Component {
+  state = {
+    items: [],
+    currentItem: { text: '', key: '' }
+  }
+
+  handleInput = event => {
+    console.log('hello input')
+  }
+
+  addItem = event => {
+    event.preventDefault()
+    console.log('hello add item')
+  }
+
+  inputElement = React.createRef();
+
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
       </div>
     );
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 class TodoList extends Component {
+    componentDidUpdate() {
+        this.props.inputElement.current.focus();
+    }
+
     render() {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="task" />
-                        <button type="submit">add task</button>
+                    <form onSubmit={this.props.addItem}>
+                        <input
+                            placeholder="task"
+                            ref={this.props.inputElement}
+                            value={this.props.currentItem.text}
+                            onChange={this.props.handleInput}
+                        />
+                        <button
+                            type="submit"
+                        >
+                            add task
+                        </button>
                     </form>
 
                 </div>
